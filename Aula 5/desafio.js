315e78107b74c4efaacfa8172eecd44704da5d72
arthur5040 <arthursaubara@gmail.com>
2024-09-16 10:57:50 -0300

    desafio em aula

---
+++ Aula 5/desafio.js
@@ -0,0 +1,13 @@
+function func1(x , y){
+    return x + y;
+  }
+  
+  const func3 = entrada => entrada + "é bonito demais mesmo"
+  
+  const minha_lista = [25, "casa",[34,12], 9.65]
+  
+  const meu_desafio= [25, "casa",[34,12], 9.65]
+  function func4(a , b ){
+    console.log(a , b);
+  }
+  
